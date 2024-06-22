1bbcc2039d353b5f763e5ac0c80dc7562d504ef1
Evan Norton <evan@retro-mmo.com>
2024-06-22 05:16:51 -0400

    Fix type error

--- src/definables.ts
+++ src/definables.ts
@@ -48,7 +48,7 @@ const getToken = (): string => {
   const pieces: string[] = [];
   for (let i: number = 0; i < 20; i++) {
     pieces.push(
-      validIDCharacters[Math.floor(Math.random() * validIDCharacters.length)],
+      validIDCharacters[Math.floor(Math.random() * validIDCharacters.length)] as string,
     );
   }
   return pieces.join("");
